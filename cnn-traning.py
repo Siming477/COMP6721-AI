672baf716356dc8e32dc449cb1ab57c9dae64339
siming477 <hsim47@163.com>
2021-03-09 15:04:11 -0500

    add evaluation

--- cnn-traning.py
+++ cnn-traning.py
@@ -3,8 +3,10 @@ import torch.nn as nn
 import torchvision
 from torchvision.transforms import transforms
 from torch.utils.data import DataLoader
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 
-# Transforms
+# Data Transforms
 transformer = transforms.Compose([
     transforms.Resize((256, 256)),
     transforms.RandomHorizontalFlip(),
@@ -12,7 +14,7 @@ transformer = transforms.Compose([
     transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
 ])
 
-# DataLoader
+# Data Loader
 train_path = 'Database/training-data'
 test_path = 'Database/training-data'
 
@@ -31,18 +33,18 @@ class CNN(nn.Module):
         super(CNN, self).__init__()
         self.conv1 = nn.Sequential(         # input shape (3, 256, 256)
             nn.Conv2d(
-                in_channels=3,              # input height
+                in_channels=3,              # rgb
                 out_channels=16,            # n_filters
                 kernel_size=3,              # filter size
                 stride=1,                   # filter movement
                 padding=1,                  # padding=(kernel_size-1)/2
             ),                              # output shape (16, 256, 256)
             nn.ReLU(),                      # activation
-            nn.MaxPool2d(kernel_size=2),    # choose max value in 2x2 area, output shape (16, 128, 128)
+            nn.MaxPool2d(kernel_size=2),    # output shape (16, 128, 128)
         )
         self.conv2 = nn.Sequential(         # input shape (16, 128, 128)
             nn.Conv2d(16, 32, 3, 1, 1),     # output shape (32, 128, 128)
-            nn.ReLU(),                      # activation
+            nn.ReLU(),
             nn.MaxPool2d(2),                # output shape (32, 64, 64)
         )
         self.out = nn.Linear(32 * 64 * 64, 3)   # fully connected layer, output 3 classes
@@ -54,7 +56,7 @@ class CNN(nn.Module):
         output = self.out(x)
         return output
 
-
+# Print CNN
 cnn = CNN()
 print(cnn)
 
@@ -62,9 +64,9 @@ print(cnn)
 optimizer = torch.optim.Adam(cnn.parameters(), lr=0.001)   # optimize all cnn parameters, learning rate=0.001
 loss_func = nn.CrossEntropyLoss()
 
-# Training and Testing
+# Training
+cnn.train()
 for epoch in range(10):
-    cnn.train()
     for i, (images, labels) in enumerate(train_loader):
         prediction = cnn(images)
         loss = loss_func(prediction, labels)
@@ -80,14 +82,27 @@ for epoch in range(10):
 
     print('Epoch: '+str(epoch)+' Train Loss: '+str(loss.item())+' Train Accuracy: '+str(accuracy))
 
-    cnn.eval()
-    total = 0
-    correct = 0
-    for i, (images, labels) in enumerate(test_loader):
-        prediction = cnn(images)
+# Testing
+cnn.eval()
+total = 0
+correct = 0
+for i, (images, labels) in enumerate(test_loader):
+    prediction = cnn(images)
 
-        total += labels.size(0)
-        _, predicted = torch.max(prediction.data, 1)
-        correct += (predicted == labels).sum().item()
+    total += labels.size(0)
+    _, predicted = torch.max(prediction.data, 1)
+    correct += (predicted == labels).sum().item()
+print("Test Accuracy: "+str(correct/total))
 
-    print("Test Accuracy: "+str(correct/total))
+# Evaluation
+y_true = []
+y_pred = []
+for data, target in test_loader:
+    for label in target.data.numpy():
+        y_true.append(label)
+    for prediction in cnn(data).data.numpy().argmax(1):
+        y_pred.append(prediction)
+print("Classification Report: ")
+print(classification_report(y_true, y_pred))
+print("Confusion Matrix: ")
+print(confusion_matrix(y_true, y_pred))
